b9b61d4853c5d34c0ef11e6ab95345a16c444986
Nidhikanwar14 <nidhikanwar45@gmail.com>
2022-06-17 23:51:21 +0530

    Evaluation_task files

---
+++ JAVA/eval_test2.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+  loadData();
+ 
+});
+
+function loadData() {
+  $('#table').append('<tr><td>' + localStorage.name +
+                    '</td><td>'+localStorage.age+
+                    '</td><td>'+ localStorage.email+
+                    '</td><td>'+  localStorage.Phone_num+
+                    '</td><td>'+ localStorage.gender+
+                    '</td><td>'+ localStorage.monday +
+                    '</td><td>'+ localStorage.tuesday +
+                    '</td><td>'+ localStorage.wednesday+
+                    '</td><td>'+localStorage.thursday+
+                    '</td><td>'+localStorage.friday+
+                    '</td><td>'+ localStorage.saturday+
+                    '</td><td>'+ localStorage.sunday+
+                    '</td></tr>');   
+}
